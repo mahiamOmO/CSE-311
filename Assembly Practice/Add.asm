b4f9924b96a86f4110f017d34b1a6a0f6772ba17
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-10-15 11:09:47 +0600

    Add files via upload

---
+++ Assembly Practice/Add.asm
@@ -0,0 +1,15 @@
+org 100h         
+
+MOV AX, 10       ; Load 10 into AX
+MOV BX, 5        ; Load 5 into BX
+
+ADD AX, BX       ; Add BX to AX (AX = AX + BX)
+
+HLT              ; Halt the program
+
+ret
+
+
+
+
+                              
